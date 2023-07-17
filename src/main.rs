bf4f5bd104d55b65c1acf603a888cf0ac2c76b78
Wesley Moore <wes@wezm.net>
2023-07-18 08:28:49 +1000

    Add option to list components

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use reqwest::Url;
 use scraper::{Html, Selector};
+use std::env;
 use std::fmt::{self, Formatter};
 
 #[derive(Debug, Eq, PartialEq, Copy, Clone)]
@@ -27,7 +28,7 @@ async fn main() {
 
     let links = Selector::parse("a[href*='/cgi-bin/redirect.cgi'][alt]").unwrap();
 
-    for (component, q, reference) in [
+    let components = [
         (Cpu, "AMD Ryzen 9 7950X", 860_00i32),
         (Motherboard, "Gigabyte X670 AORUS ELITE AX", 450_00),
         (Graphics, "Gigabyte Radeon RX 6700 XT EAGLE", 478_00),
@@ -38,7 +39,18 @@ async fn main() {
         (PowerSupply, "Corsair CP-9020199-AU", 149_00),
         (CPUCooling, "Noctua NH-D15 CPU Cooler -NH-D15S", 146_00),
         (Keyboard, "KBKCQ3N3BROWN", 279_00),
-    ] {
+    ];
+
+    if let Some("-l") = env::args_os()
+        .skip(1)
+        .next()
+        .as_deref()
+        .and_then(|s| s.to_str())
+    {
+        return list_components(&components);
+    }
+
+    for (component, q, reference) in components {
         // We do them in sequence because StaticICE limits concurrent requests to 3
         let res = search(component, q).await;
         let doc = Html::parse_document(&res.page);
@@ -98,5 +110,11 @@ impl fmt::Display for Component {
             Component::PowerSupply => f.write_str("Power Supply"),
             Component::Ssd => f.write_str("Primary SSD"),
         }
+    }
+}
+
+fn list_components(components: &[(Component, &'static str, i32)]) {
+    for (component, name, price) in components {
+        println!("{}: {} - ${:.02}", component, name, *price as f64 / 100.);
     }
 }
